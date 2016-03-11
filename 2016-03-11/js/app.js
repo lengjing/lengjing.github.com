46b3a80af760e37a89c3d2ce0ea0114f047d5d31
lengjing <qq.com>
2016-03-12 00:43:38 +0800

    update

--- 2016-03-11/js/app.js
+++ 2016-03-11/js/app.js
@@ -1,93 +1,207 @@
 var app = app || {};
 
 app.init = function(){
-    this.initConsole();
+    $.ajaxSetup({
+      contentType: "application/json"
+    })
+    var consolerObj = this.initConsoler();
+    this.initHangman(consolerObj);
 };
 
-app.initConsole = function(){
-    var commands = {
-        help: 'help',
-        clear: 'clear',
-        exit: 'exit',
-        man: 'man cookies',
-        start: 'start game'
-    };
-    var helloMessage = "this is helloMessage";
-    var helpMessage = "Type `" + commands.man + "' to learn more.\nUse `" + commands.exit + "' to close.";
-    var errorMessage = "Command not found. Type `" + commands.help + "` to see all commands.";
-
+app.initConsoler = function(){
     var $terminal = $('<div/>').addClass("hangman").appendTo("body");
-
-    var controller = $terminal.console({
-        autofocus: true,
-        scrollArea: $terminal,
-        promptHistory: true,
-        promptLabel: '~ root$ ',
-        welcomeMessage: helloMessage + "\n"+ helpMessage,
-
-        onEsc: function () {
-            bannerInstance.closeHandler.call(bannersRotator.instances[0]);
-        },
-
-        commandValidate: function (line) {
-            return line != "";
-        },
-
-        commandHandle: function (line) {
-            switch (line) {
-                case (commands.man):
-                    this.expandFrame();
-                    return '\nA cookie is a small piece of data sent by a website to your browser. It helps the website to remember information about your visit, like your country and other settings. That can make your next visit easier and the site more useful to you.\n\nWe use cookies on some (but not all) pages to deliver personalized content or to tailor our information offerings or responses according to the way you use the site, and/or your current context on the site. We do not use cookies to gather or transmit any personally identifiable information about you.\n\nRead our <a href="/company/privacy.html" class="cookies-notify__link">privacy policy</a> for a detailed explanation on how we protect your privacy in our use of cookies and other information.\n \n';
-
-                case (commands.help):
-                    return helpMessage;
-
-                case (commands.clear):
-                    $terminal.css('height', '30%');
-                    controller.reset();
-                    return '';
-
-                case (commands.exit):
-                    this.onEsc();
-                    return line;
-
-                case commands.start:
-                    this.startGame();
-                    return line;
-                default:
-                    return errorMessage;
-            }
-        },
-
-        charInsertTrigger: function (keycode, line) {
-            return (keycode < 48 || keycode > 57);
-        },
-
-        completeHandle: function (prefix) {
-            var complete = [];
-
-            for (var command in commands) {
-                if (commands.hasOwnProperty(command)) {
-
-                    var item = commands[command];
-
-                    if (item.lastIndexOf(prefix, 0) === 0) {
-                        complete.push(item.substring(prefix.length));
-                    }
-                }
-            }
-
-            return complete;
-        },
-
-        expandFrame: function () {
-            $terminal.css('height', '430px');
-        },
-        startGame: function(){
-
-        }
-    });
-
-    return controller;
+    var config = {
+      autofocus: true,
+      scrollArea: $terminal,
+      promptHistory: true,
+      promptLabel: '~ root$ '
+    };
+    return {
+      consoler: $terminal.console(config),
+      config: config
+    }
 }
+app.initHangman = function(consolerObj){
+    var commands = {
+        game: 'hangman',
+        start: 'start',
+        exit: 'exit',
+        fetch: 'give me a word',
+        result: 'get result',
+        submit: 'submit result',
+        help: 'help'
+    };
+    var messages = {
+        hello: "Wellcome to the hangman game!",
+        help: "Type `" + commands.game + "' to learn more.\n",
+        error: "Command not found. Type `" + commands.help + "` to see all commands.",
+        commands: "Type `" + commands.start + "` to start game.\n" +
+                  "Type `" + commands.fetch + "` to ask for a word from the server.\n" +
+                  "Type `" + commands.result + "` to get the result.\n" +
+                  "Type `" + commands.submit + "` to get your result at anytime after starting the game.\n"
+    };
 
+    var consoler = consolerObj.consoler;
+    var instance = {
+      url: "https://strikingly-hangman.herokuapp.com/game/on",
+      step: -1,
+      _action: ["startGame", "nextWord", "guessWord", "getResult", "submitResult"],
+      action: function(){
+        return this._action[this.step];
+      }
+    };
+
+    $.extend(consolerObj.config, {
+      commandValidate: function (line) {
+          return line != "";
+      },
+      commandHandle: function (line, report) {
+          if(consoler.command === false){
+            return this.handleInput(line, report);
+          }
+          switch (line) {
+              case commands.game:
+                  return messages.commands;
+              case commands.help:
+                  return messages.help;
+              case commands.clear:
+                  controller.reset();
+                  return '';
+              case commands.start:
+                  return this.startGame();
+              case commands.fetch:
+                  return this.fetch(line, report);
+              case commands.result:
+                  return this.result(line, report);
+              case commands.submit:
+                  return this.submit(line, report);
+              default:
+                  return messages.error;
+          }
+
+      },
+      completeHandle: function (prefix) {
+          var complete = [];
+
+          for (var command in commands) {
+              if (commands.hasOwnProperty(command)) {
+
+                  var item = commands[command];
+
+                  if (item.lastIndexOf(prefix, 0) === 0) {
+                      complete.push(item.substring(prefix.length));
+                  }
+              }
+          }
+
+          return complete;
+      },
+      cancelHandle: function(){
+        consoler.promptLabel = '~ root$ ';
+        consoler.command = true;
+        consoler.report('');
+      },
+      startGame: function(line){
+          if(instance.step > -1){
+              return "You are already in the game."
+          }
+          consoler.command = false;
+          instance.step = 0;
+
+          return "Please type your playerId (you can get it from your inviation email. here your can type js_shi@126.com)";
+      },
+      handleInput: function(line, report){
+          switch (instance.step) {
+            case 0:
+              this.inputStep1(line, report);
+              break;
+            case 2:
+              this.inputStep2(line, report);
+          }
+      },
+      fetch: function(line, report){
+          instance.step = 1;
+          $.ajax({
+            url: instance.url,
+            data: JSON.stringify({"sessionId": instance.sessionId, "action": instance.action()}),
+            type: "post"
+          }).then(function(data){
+            data = data.data;
+            instance.step = 2;
+            consoler.command = false;
+            consoler.promptLabel = "> ";
+            (instance.words || (instance.words = [])).push(data.data);
+
+            var msg = "word: " + data.word + "\n" +
+                   "you have tried " + data.totalWordCount + "\n" +
+                   "the number of wrong guess you already made on this word is " + data. wrongGuessCountOfCurrentWord + "\n" +
+                   "please type your word.(you can type ctrl + c to exit)"
+            report(msg);
+          }).fail(function(){
+            report("Please try again")
+          })
+      },
+      inputStep1: function(line, report){
+        instance.playerId = line;
+        $.ajax({
+          url: instance.url,
+          data: JSON.stringify({"playerId": instance.playerId, "action": instance.action()}),
+          type: "post"
+        }).then(function(data){
+          consoler.command = true;
+          instance.step = 1;
+          instance.sessionId = data.sessionId;
+
+          report(data.message + ",please type `" + commands.fetch + "` to ask for a word from the server.\n")
+        }).fail(function(d,e,m){
+          report(m + " please type your playerId again.")
+        })
+      },
+      inputStep2: function(line, report){
+        $.ajax({
+          url: instance.url,
+          data: JSON.stringify({"sessionId": instance.sessionId, "action": instance.action(), "guess": line}),
+          type: "post"
+        }).then(function(data){
+          data = data.data;
+
+          var msg = "word: " + data.word + "\n" +
+                 "you have tried " + data.totalWordCount + "\n" +
+                 "the number of wrong guess you already made on this word is " + data. wrongGuessCountOfCurrentWord + "\n";
+          report(msg + (data.word.indexOf("*") === -1 ? "you guess the word correctly" : ""));
+        }).fail(function(d){
+          report(d.responseJSON.message || "Please try it again.")
+        })
+      },
+      result: function(line, report){
+        instance.step = 3;
+        $.ajax({
+          url: instance.url,
+          data: JSON.stringify({"sessionId": instance.sessionId, "action": instance.action()}),
+          type: "post"
+        }).then(function(data){
+          data = data.data;
+          var msg = "the total number of words you tried is " + data.totalWordCount + "\n" +
+                    "the total number of words you guess correctly is " + data.correctWordCount + "\n" +
+                    "the total number of Wrong guess you have made is" + data.totalWrongGuessCount + "\n" +
+                    "your score is " + data.score;
+          report(msg);
+        }).fail(function(){
+          report("Please try it again.")
+        })
+      },
+      submit: function(line, report){
+        instance.step = 4;
+
+        $.ajax({
+          url: instance.url,
+          data: JSON.stringify({"sessionId": instance.sessionId, "action": instance.action()}),
+          type: "post"
+        }).then(function(data){
+          report("GAME OVER");
+        }).fail(function(){
+          report("Please try it again.")
+        })
+      }
+    })
+}
